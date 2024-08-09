890158657e8edaafed9443af9d2618ba97adb2ae
sfisher <scott.fisher@ucop.edu>
2024-08-09 13:46:41 -0700

    Add argument parsing for settings like where to listen

--- gradiodemo_chatgpt/app_csv.py
+++ gradiodemo_chatgpt/app_csv.py
@@ -4,6 +4,7 @@ import markdown
 import pdb
 import open_api_code
 import local_secrets
+import argparse
 
 # looking at https://www.cloudskillsboost.google/course_templates/552?utm_campaign=FY24-Q2-global-website-skillsboost&utm_content=developers&utm_medium=et&utm_source=cgc-site&utm_term=-
 # vertex ai studio
@@ -12,6 +13,7 @@ import local_secrets
 
 # pip install --upgrade google-cloud-aiplatform
 # gcloud auth application-default login
+
 
 def process_file_and_return_markdown(file, system_info, prompt):
     # Read the file
@@ -25,27 +27,40 @@ def process_file_and_return_markdown(file, system_info, prompt):
     return response
 
 
-# Create the Gradio interface
-# iface = gr.Interface(fn=process_file_and_return_markdown, inputs="file", outputs="markdown")
+def main():
+    parser = argparse.ArgumentParser(description="Run the Gradio app with specified options.")
+    parser.add_argument('--listen', type=str, default="127.0.0.1",
+                        help="If set, the app will listen on the the IP address.")
+    parser.add_argument('--port', type=int, default=7860, help="The port to run the app on.")
+    parser.add_argument('--share', action='store_true', help="If set, the app will create a public link.")
+    parser.add_argument('--user', type=str, help="Username for authentication.")
+    parser.add_argument('--password', type=str, help="Password for authentication.")
+    parser.add_argument('--debug', action='store_true', help="If set, the app will run in debug mode.")
 
-default_system_info =\
-    ("You are a system helping a researcher analyze a file containing research data in tabular format. "
-     "The objective is to give advice to improve the data quality for reuse and reproducibility by other researchers "
-     "in the same field. Besides general practices, specific advice for improving the given file is most useful. "
-     "Notify the user if the file does not appear to be tabular data.")
+    args = parser.parse_args()
 
-default_user_prompt = (
-    'The file contains research data in tabular format. Analyze it for data '
-    'quality metrics and to help visualize the data for other researchers in the same field. How can it be '
-    'improved for reuse and reproducibility?')
+    # Create the Gradio interface
+    # iface = gr.Interface(fn=process_file_and_return_markdown, inputs="file", outputs="markdown")
 
-# Create the Gradio interface with additional text inputs
-iface = gr.Interface(
-    fn=process_file_and_return_markdown,
-    inputs=["file", gr.TextArea(label="System conditioning", value=default_system_info),
-            gr.TextArea(label="User prompt", value=default_user_prompt)],
-    outputs="markdown",
-    title="Basic data quality analysis with ChatGPT"
-)
+    default_system_info =\
+        ("You are a system helping a researcher analyze a file containing research data in tabular format. "
+         "The objective is to give advice to improve the data quality for reuse and reproducibility by other researchers "
+         "in the same field. Besides general practices, specific advice for improving the given file is most useful. "
+         "Notify the user if the file does not appear to be tabular data.")
 
-iface.launch(debug=True)
+    default_user_prompt = (
+        'The file contains research data in tabular format. Analyze it for data '
+        'quality metrics and to help visualize the data for other researchers in the same field. How can it be '
+        'improved for reuse and reproducibility?')
+
+    # Create the Gradio interface with additional text inputs
+    iface = gr.Interface(
+        fn=process_file_and_return_markdown,
+        inputs=["file", gr.TextArea(label="System conditioning", value=default_system_info),
+                gr.TextArea(label="User prompt", value=default_user_prompt)],
+        outputs="markdown",
+        title="Basic data quality analysis with ChatGPT"
+    )
+
+    iface.launch(debug=args.debug, share=args.share, username=args.user, password=args.password, host=args.listen,
+                 port=args.port)
