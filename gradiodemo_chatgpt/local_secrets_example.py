da88ffe53f1611d67a7c8ab74249cd7f2690cb2b
sfisher <scott.fisher@ucop.edu>
2024-08-27 12:08:57 -0700

    Removing individual chatGPT/Gemini demos since we have one that does both now.

--- gradiodemo_chatgpt/local_secrets_example.py
+++
@@ -1,5 +0,0 @@
-PROJECT = "meowing-rabbit-123456-g0"
-LOCATION = "us-central1"
-
-# it still seems like there should be a way to use the API key instead of the gcloud tools and a local authentication
-GOOG_API_KEY = "AIbooboo"
